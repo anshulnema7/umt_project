81dbba4bc1deaf49c1db4cba5b3b295a93b494d1
anema <anshulnema7@gmail.com>
2017-08-22 11:59:29 +0530

    Merge origin/master
    Conflicts:
    	web/PendingRequest.jsp

--- src/java/com/test/test.java
+++ src/java/com/test/test.java
@@ -36,10 +36,7 @@ public class test extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet test</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet test at " + request.getContextPath() + "</h1>");
+           
             out.println("</body>");
             out.println("</html>");
         } finally {
