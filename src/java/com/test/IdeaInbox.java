3f2d93e0123eab0e71f6744f3161c85eab12ec3b
anema <anshulnema7@gmail.com>
2017-08-24 11:59:33 +0530

    Merge origin/master
    Conflicts:
    	web/PendingRequest.jsp

--- src/java/com/test/IdeaInbox.java
+++ src/java/com/test/IdeaInbox.java
@@ -9,18 +9,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.reflect.TypeToken;
-import javax.swing.JOptionPane;
 
 /**
  *
